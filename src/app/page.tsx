14c063ab1b295abf603e4743a4129550ade47a78
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2025-04-12 18:28:56 +0200

    Copy tools usage from the ai sdk tutorial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,12 @@ export default function Chat() {
             switch (part.type) {
               case "text":
                 return <div key={`${message.id}-${i}`}>{part.text}</div>;
+              case "tool-invocation":
+                return (
+                  <pre key={`${message.id}-${i}`}>
+                    {JSON.stringify(part.toolInvocation, null, 2)}
+                  </pre>
+                );
             }
           })}
         </div>
