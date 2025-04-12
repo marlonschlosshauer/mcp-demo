d7294f700596e96c05d52df72f66e2fbe7e4d5d6
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2025-04-12 19:19:05 +0200

    Early WIP

---
+++ src/lib/tools/contentful.ts
@@ -0,0 +1,19 @@
+import { createTransport } from "@smithery/sdk/transport.js";
+import { experimental_createMCPClient } from "ai";
+
+export const getContentfulMcp = async () => {
+  const transport = createTransport(
+    "https://server.smithery.ai/@ivotoby/contentful-management-mcp-server",
+    {
+      host: "https://api.contentful.com",
+      spaceId: process.env.CONTENTFUL_SPACE_ID ?? "",
+      environmentId: process.env.CONTENTFUL_ENVIRONMENT_ID ?? "",
+      managementToken: process.env.CONTENTFUL_CMA_TOKEN ?? "",
+    },
+    process.env.SMITHERY_API_KEY ?? "",
+  );
+
+  return await experimental_createMCPClient({
+    transport,
+  });
+};
