27937c1d20c0d8c1b452a92c124a1404e00a6923
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2025-04-26 20:43:41 +0200

    Integrate user input into the chat

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,17 +1,16 @@
 "use client";
 
 import { useChat } from "@ai-sdk/react";
-import { useOptions } from "@/lib/options";
 import { Message } from "@/components/chat/message/Message";
 import { Loading } from "@/components/chat/message/Loading";
+import { Entry } from "@/components/chat/entry/Entry";
 
 export default function Chat() {
-  const { model } = useOptions();
-  const { messages, input, handleInputChange, handleSubmit, status } = useChat({
+  const data = useChat({
     maxSteps: 5,
   });
 
-  console.log(messages);
+  const { messages, status } = data;
 
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch gap-4">
@@ -19,16 +18,7 @@ export default function Chat() {
         <Message key={key} {...message} />
       ))}
       {status === "submitted" && <Loading />}
-      <form onSubmit={(e) => handleSubmit(e, { body: { model } })}>
-        <input
-          autoFocus
-          disabled={status === "submitted" || status === "streaming"}
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-zinc-300 rounded shadow-xl bg-white disabled:bg-gray-500 disabled:text-gray-50 disabled:cursor-progress"
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
-        />
-      </form>
+      {status !== "submitted" && status !== "streaming" && <Entry {...data} />}
     </div>
   );
 }
---
+++ src/components/chat/entry/Entry.tsx
@@ -0,0 +1,32 @@
+import { useOptions } from "@/lib/options";
+import { useChat } from "@ai-sdk/react";
+import React from "react";
+import { Wrapper } from "../message/Wrapper";
+
+export type EntryProps = ReturnType<typeof useChat>;
+
+export const Entry: React.FC<EntryProps> = ({
+  input,
+  handleSubmit,
+  handleInputChange,
+  status,
+}) => {
+  const { model } = useOptions();
+  return (
+    <div className="whitespace-pre-wrap">
+      <Wrapper isUser={true}>
+        <form onSubmit={(e) => handleSubmit(e, { body: { model } })}>
+          <input
+            autoFocus
+            disabled={status === "submitted" || status === "streaming"}
+            className="w-full outline-none resize-none px-3"
+            value={input}
+            placeholder="Say something..."
+            onChange={handleInputChange}
+            style={{ height: "auto" }}
+          />
+        </form>
+      </Wrapper>
+    </div>
+  );
+};
